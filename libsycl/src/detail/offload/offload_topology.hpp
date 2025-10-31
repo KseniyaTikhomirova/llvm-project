f51c6df68be8c46059d498976b2cce869db92bfc
Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>
2025-10-31 07:05:16 -0700

    remove index range
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- libsycl/src/detail/offload/offload_topology.hpp
+++ libsycl/src/detail/offload/offload_topology.hpp
@@ -37,26 +37,21 @@ using PlatformWithDevStorageType =
 // Contiguous global storage of platform handlers and device handles (grouped by
 // platform) for a backend.
 struct OffloadTopology {
-  OffloadTopology() : OlBackend(OL_PLATFORM_BACKEND_UNKNOWN) {}
-  OffloadTopology(ol_platform_backend_t OlBackend) : OlBackend(OlBackend) {}
+  OffloadTopology() : MBackend(OL_PLATFORM_BACKEND_UNKNOWN) {}
+  OffloadTopology(ol_platform_backend_t OlBackend) : MBackend(OlBackend) {}
 
-  void set_backend(ol_platform_backend_t B) { OlBackend = B; }
+  void set_backend(ol_platform_backend_t B) { MBackend = B; }
 
   // Platforms for this backend
   range_view<ol_platform_handle_t> platforms() const {
-    return {Platforms.data(), Platforms.size()};
+    return {MPlatforms.data(), MPlatforms.size()};
   }
 
   // Devices for a specific platform (platform_id is index into Platforms)
   range_view<ol_device_handle_t> devicesForPlatform(size_t PlatformId) const {
-    if (PlatformId >= PlatformDevices.size())
+    if (PlatformId >= MDevRangePerPlatformId.size())
       return {nullptr, 0};
-    return PlatformDevices[PlatformId].first;
-  }
-
-  size_t getFirstDeviceIndexForPlatform(size_t PlatformId) const {
-    assert(PlatformId < PlatformDevices.size());
-    return PlatformDevices[PlatformId].second;
+    return MDevRangePerPlatformId[PlatformId];
   }
 
   // Register new platform and devices into this topology under that platform.
@@ -66,36 +61,34 @@ struct OffloadTopology {
     if (!PlatformsAndDev.size())
       return;
 
-    Platforms.reserve(PlatformsAndDev.size());
-    PlatformDevices.reserve(Platforms.size());
-    Devices.reserve(TotalDevCount);
+    MPlatforms.reserve(PlatformsAndDev.size());
+    MDevRangePerPlatformId.reserve(MPlatforms.size());
+    MDevices.reserve(TotalDevCount);
 
     for (auto &[NewPlatform, NewDevs] : PlatformsAndDev) {
-      Platforms.push_back(NewPlatform);
-
-      size_t StartIdx = Devices.size();
-      range_view<ol_device_handle_t> R{Devices.data() + Devices.size(),
+      MPlatforms.push_back(NewPlatform);
+      range_view<ol_device_handle_t> R{MDevices.data() + MDevices.size(),
                                        NewDevs.size()};
-      Devices.insert(Devices.end(), NewDevs.begin(), NewDevs.end());
-      PlatformDevices.push_back({R, StartIdx});
+      MDevices.insert(MDevices.end(), NewDevs.begin(), NewDevs.end());
+      MDevRangePerPlatformId.push_back(R);
     }
 
-    assert(TotalDevCount == Devices.size());
+    assert(TotalDevCount == MDevices.size());
   }
 
-  ol_platform_backend_t backend() { return OlBackend; }
+  ol_platform_backend_t backend() { return MBackend; }
 
 private:
-  ol_platform_backend_t OlBackend = OL_PLATFORM_BACKEND_UNKNOWN;
+  ol_platform_backend_t MBackend = OL_PLATFORM_BACKEND_UNKNOWN;
 
   // Platforms and devices belonging to this backend (flattened)
-  std::vector<ol_platform_handle_t> Platforms;
-  std::vector<ol_device_handle_t> Devices; // sorted by platform
+  std::vector<ol_platform_handle_t> MPlatforms;
+  std::vector<ol_device_handle_t> MDevices; // sorted by platform
 
   // Vector holding range of devices for each platform (index is platform index
   // within Platforms)
-  std::vector<std::pair<range_view<ol_device_handle_t>, size_t>>
-      PlatformDevices; // PlatformDevices.size() == Platforms.size()
+  std::vector<range_view<ol_device_handle_t>>
+      MDevRangePerPlatformId; // PlatformDevices.size() == Platforms.size()
 };
 
 // Initialize the topologies by calling olIterateDevices.
